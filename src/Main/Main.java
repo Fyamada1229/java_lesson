f6bd4858245e42a7d660c8664b3ef83419530c76
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-01-31 09:18:25 +0900

    add

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,19 +1,28 @@
 package Main;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.FlowLayout;
 
-
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class Main {
 	
 	public static void main(String[] args) {
-		try {
-			FileWriter fw = new FileWriter("data.tex");
-		} catch (IOException e) {
-			System.out.println("エラーが発生しました");
-			System.exit(1);
-		}
-		 
-	
+		JFrame frame = new JFrame("はじめてのGET");
+		JLabel label = new JLabel("Hello World");
+		JLabel label2 = new JLabel("yamada");
+		JButton button = new JButton("押してね");
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(label);
+		frame.getContentPane().add(label2);
+		frame.getContentPane().add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,100);
+		frame.setVisible(true);
+		
+		
+		
+		
+		
 	}
 }

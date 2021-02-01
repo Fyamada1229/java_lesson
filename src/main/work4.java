2f3e713f82f94535efc94f31ddc9d57de45fdbd7
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-02-01 10:22:16 +0900

    add

---
+++ src/main/work4.java
@@ -0,0 +1,15 @@
+package main;
+
+public class work4 {
+
+	public static void main(String[] args) {
+		
+		byte b = 10;
+		double d = 10.5;
+		d += 190.5;
+		char c = 'A';
+		System.out.println("変数bの値は" + b);
+		System.out.println("変数bの値は" + d);
+		System.out.println("変数bの値は" + c);
+	}
+}

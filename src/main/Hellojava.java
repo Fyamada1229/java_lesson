e2ac77ce152221d4ff1fdc245874bbc9f5939094
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-02-02 10:57:42 +0900

    for lesson

--- src/main/Hellojava.java
+++ src/main/Hellojava.java
@@ -3,7 +3,21 @@ package main;
 public class Hellojava {
 
 	public static void main(String[] args) {
-		System.out.println("Hellor world");
-	}
-
+		
+		
+		int [] ary = {10,20,30,44,55,101};
+		
+		for (int i: ary) {
+			if(( i % 2 )==0) {
+				System.out.println("配列の値は" + i);
+			} else {
+				System.out.println("奇数です" + i);
+			}
+		}
+		
+		
+		
+		
+		}		
+		
 }

6c780e2a4fe4db98dd1653b389b82d42f8921326
Fyamada1229 <yamadafutosi1229@gmail.com>
2021-02-02 09:26:45 +0900

    add

--- src/main/work4.java
+++ src/main/work4.java
@@ -1,15 +1,19 @@
 package main;
 
+
 public class work4 {
 
 	public static void main(String[] args) {
-		
-		byte b = 10;
-		double d = 10.5;
-		d += 190.5;
-		char c = 'A';
-		System.out.println("変数bの値は" + b);
-		System.out.println("変数bの値は" + d);
-		System.out.println("変数bの値は" + c);
+		String s = "ABC";
+		switch(s) {
+			case "ABC":
+				System.out.println("ABCです");
+				
+			case "DEF": 
+				System.out.println("DEFです");
+				break;
+			default:
+				System.out.println("それ以外です");
+		}
 	}
 }
